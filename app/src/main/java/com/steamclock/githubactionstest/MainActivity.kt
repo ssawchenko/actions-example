968ef548decf1d04490a01544fe44a0a2e483d14
Shayla Sawchenko <shayla.sawchenko@recursecreative.com>
2021-08-13 09:26:27 -0700

    first test

--- app/src/main/java/com/steamclock/githubactionstest/MainActivity.kt
+++ app/src/main/java/com/steamclock/githubactionstest/MainActivity.kt
@@ -14,6 +14,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        //changed
+
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
